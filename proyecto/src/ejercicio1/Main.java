e65e5b6dc8c663f73232578a2685fc69fb754e2b
raulalbiol <raul.albiol@thepower.education>
2025-05-14 16:36:45 +0200

    Ejercicio1 y enunciado2

--- proyecto/src/ejercicio1/Main.java
+++ proyecto/src/ejercicio1/Main.java
@@ -1,5 +1,7 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -35,13 +37,46 @@ public class Main {
         *   4º Comrpobar que con a, b y c pueda formar un triangulo.  a + b > c también a + c > b y también b + c > a
         *   5º En caso de no se pudiera mostraremos el texto: "No es un triangulo válido".
         *   6º En caso de que si se pudiera comprobaremos el tipo de triangulo.
-        *   7º Comprobamos si es un triangulo equilatera. a = b = c
+        *   7º Comprobamos si es un triangulo equilatero. a = b = c
         *   8º Comprobamos si es un triangulo isosceles. a = b o a = c o b = c
         *   9º Comprobamos si es escaleno. Todos los lados son diferentes.
         *   10º Comprobamos si es un triangulo rectángulo (Teorema pitagoras) a*a + b*b = c*c
         *
         * FIN
         * */
+
+
+        Scanner scn = new Scanner(System.in);
+        int a, b, c;
+
+        System.out.println("Introduzca el primer lado");
+        a = scn.nextInt();
+
+        System.out.println("Introduzca el segundo lado");
+        b = scn.nextInt();
+
+        System.out.println("Introduzca el tercer lado");
+        c = scn.nextInt();
+
+        if(a + b > c && a + c > b && b + c > a){
+            System.out.println("Es un triangulo válido");
+            if(a == b && b == c){ //a = b = c
+                System.out.println("Es un triángulo equilatero");
+            }else if(a == b || a == c || b == c){
+                System.out.println("Es un triángulo isósceles");
+            }else{
+                System.out.println("Es un triángulo escaleno");
+            }
+
+            if((a*a) + (b*b) == (c*c) || (a*a) + (c*c) == (b*b) || (b*b) + (c*c) == (a*a)) {
+                System.out.println("Además es un triangulo rectangulo");
+            }
+            
+        }else{
+            System.out.println("No es un triangulo válido");
+        }
+
+
 
     }
 
---
+++ proyecto/src/ejercicio2/Main.java
@@ -0,0 +1,44 @@
+package ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        *
+        * Crear un programa en Java que lea una cadena de texto y convierta cada carácter en un número según su posición en la cadena y su código ASCII. El cifrado será una secuencia de números separados por guiones (-).
+        Instrucciones:
+        *
+        El programa debe pedir al usuario una cadena de texto.
+
+        Recorre la cadena carácter por carácter usando un bucle for.
+
+        Para cada carácter:
+
+        Calcula su código ASCII (int codigo = (int) caracter;).
+
+        Si la posición del carácter es par, usa un while para sumar un valor extra a su código (por ejemplo, suma 1 tantas veces como la posición indique).
+
+        Si la posición es impar, réstale 1 directamente al código.
+
+        Une todos los valores cifrados en un solo texto, separados por guiones (-).
+
+        Muestra la cadena cifrada al usuario.
+
+        Usa un bucle do-while para permitir cifrar otra cadena hasta que el usuario escriba "STOP".
+
+        Ejemplo de ejecución:
+
+        Introduce una cadena a cifrar:
+        Hola
+        Cadena cifrada: 73-110-111-100
+
+        ¿Quieres cifrar otra cadena? (escribe STOP para salir):
+        Hi
+        Cadena cifrada: 73-104
+
+        ¿Quieres cifrar otra cadena? (escribe STOP para salir):
+        STOP
+        Programa terminado.
+        *
+        * */
+    }
+}
