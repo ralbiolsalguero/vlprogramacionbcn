39845c62ba3f53d89539a8afcc3fb6c6bab59ec3
raulalbiol <raul.albiol@thepower.education>
2025-05-14 15:58:55 +0200

    Ejercicio triangulo

---
+++ proyecto/src/ejercicio1/Main.java
@@ -0,0 +1,48 @@
+package ejercicio1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*CONDICIONALES AVANZADO*/
+        /*
+        *
+        * Escriba un programa en Java que reciba tres números enteros e identifique si pueden
+        * representar los lados de un triangulo. Si lo son determina el tipo de triangulo:
+        *
+        *  - Equilatero: Todos los lados son iguales a = b = c
+        *  - Isósceles: Dos lados son iguales
+        *  - Escaleno: Todos los lados son diferentes
+        *
+        * Y también comprobar si es un triangulo rectangulo.
+        * a*a + b*b = c*c
+        *
+        * NOTA: Triangulo valido: a, b, c.  a + b > c también a + c > b y también b + c > a
+        *
+        *
+        * */
+
+
+        /*PSEUDOCÓDIGO*/
+
+        /*
+        *
+        * INICIO
+        *
+        *   1º Introducir a
+        *   2º Introducir b
+        *   3º Introducir c
+        *   4º Comrpobar que con a, b y c pueda formar un triangulo.  a + b > c también a + c > b y también b + c > a
+        *   5º En caso de no se pudiera mostraremos el texto: "No es un triangulo válido".
+        *   6º En caso de que si se pudiera comprobaremos el tipo de triangulo.
+        *   7º Comprobamos si es un triangulo equilatera. a = b = c
+        *   8º Comprobamos si es un triangulo isosceles. a = b o a = c o b = c
+        *   9º Comprobamos si es escaleno. Todos los lados son diferentes.
+        *   10º Comprobamos si es un triangulo rectángulo (Teorema pitagoras) a*a + b*b = c*c
+        *
+        * FIN
+        * */
+
+    }
+
+}
