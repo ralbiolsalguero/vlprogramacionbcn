fe37b9801310aa30fc0917013d546608e5b6713e
raulalbiol <raul.albiol@thepower.education>
2025-06-11 16:14:57 +0200

    Ejercicio2

--- clase1106/src/ejercicio1/Main.java
+++ clase1106/src/ejercicio1/Main.java
@@ -5,23 +5,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        char [] letraDNI = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+
         Scanner scn = new Scanner(System.in);
         int dni, resto;
 
         System.out.println("Introduzca el número del DNI");
         dni = scn.nextInt();
 
-        resto = dni % 23; //resto = 14
 
-        System.out.println(letraDNI[resto]); //letraDNI[14] = Z
-        //12345678 -> Z (14)
-
-
-
-
-
+        System.out.println("Número: "+dni+" Letra: "+calcularLetra(dni));
 
 
     }
+
+    static char calcularLetra(int dni){
+        int resto;
+        char letra;
+        char [] letraDNI = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+
+        resto = dni % 23; //resto = 14
+        letra = letraDNI[resto];
+
+        return letra;
+    }
+
+    //MÉTODO QUE LE PASEMOS EL NÚMERO Y LA LETRA POR OTRO, Y TENDREMOS QUE DECIR
+    //SI EL DNI, ES CORRECTO O NO LO ES
 }
